8a2281256f67bd3959a2ca3d59ebfc39b87129c4
RPG0007 <90514460+RPG0007@users.noreply.github.com>
2022-07-24 19:10:37 +0300

    docs:solve

--- src/index.js
+++ src/index.js
@@ -35,12 +35,39 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '' :  ' '
 };
-
 function decode(expr) {
-    // write your solution here
-}
+    let dec1 = []
+    let str1 = expr
+    for(let f=0;f<expr.length;f=f+10){
+        dec1.push(str1.substring(0,10))
+        str1 = str1.slice(10)
+      
+    }  
+    str2=""
+   for (let i = 0;i<dec1.length;i++)
+   {
+       dec1[i]=dec1[i].replaceAll('10','.')
+       dec1[i]=dec1[i].replaceAll('11','-')
+       dec1[i]=dec1[i].replaceAll('0',' ')
+       dec1[i]=dec1[i].replace(/\s/g, '')
+       dec1[i]=dec1[i].replaceAll('*',' ')
+        str2=str2+dec1[i]+' '
+   }
+   str2=str2.replaceAll('          ','')
+    outPut = "";
 
+    for (var i = 0; i < str2.split(" ").length; i++) {
+        if(str2.split(" ")[i]==" "){
+          outPut+=" "
+        }
+        else{
+     outPut += MORSE_TABLE[str2.split(" ")[i]];}
+        
+    }
+    return outPut.substring(0,outPut.length-1);
+}
 module.exports = {
     decode
 }
